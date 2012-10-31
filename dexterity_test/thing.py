d5784c31bc2e210a8efc00615575fbf03a8c7dec
Alex Clark <aclark@aclark.net>
2012-10-30 22:05:39 -0400

    Ditch view; fix model name

--- dexterity_test/thing.py
+++ dexterity_test/thing.py
@@ -3,9 +3,4 @@ from plone.directives import form
 
 
 class IThing(form.Schema):
-    form.model('project.xml')
-
-    
-class View(grok.View):
-    grok.context(IThing)
-    grok.require('zope2.View')
+    form.model('thing.xml')
